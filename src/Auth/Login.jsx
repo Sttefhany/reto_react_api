c20a20ed4c7aa9666843984f9752f6cbf5b90d17
Estamany <eg6806834@gmail.com>
2026-09-23 16:06:11 -0500

    Fix: actualizar ruta a context/CartContext

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { User, LogOut, X, LogIn, Mail, AlertTriangle } from "lucide-react";
 import { useAuth } from "./AuthContext";
-import { useCart } from "../CartContext";
+import { useCart } from "../context/CartContext";
 
 export default function Login() {
   const { usuario, login, logout } = useAuth();
